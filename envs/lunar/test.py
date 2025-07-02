db1dca838cb029625f216e9d30d53aa78620047a
wstzeng <wst991002@gapp.nthu.edu.tw>
2025-07-02 21:59:48 +0800

    refine testing

--- envs/lunar/test.py
+++ envs/lunar/test.py
@@ -1,12 +1,12 @@
 import gymnasium as gym
 from policy.discrete_policy import DiscretePolicyNetwork as Policy
 from agent import ReinforceAgent as Agent
-from utils.train import train_loop
 from utils.test import test_loop
 
-def main(env_name='LunarLander-v3', T=200, N=10):
+def main(env_name='LunarLander-v3'):
     # Initialize environment to extract dimensions
-    env = gym.make(env_name)
+    env = gym.make(env_name,
+                   enable_wind=True, wind_power=20.0, turbulence_power=2.0)
     state_dim = env.observation_space.shape[0]
     n_actions = env.action_space.n
     env.close()
@@ -15,13 +15,10 @@ def main(env_name='LunarLander-v3', T=200, N=10):
     actor = Policy(state_dim, n_actions)
     agent = Agent(actor, lr=1e-3, gamma=0.99, device='cpu')
 
-    # Run training
-    train_loop(env_name, agent, T, N, monitor_mode=['cli', 'live', 'file'])
+    agent.load_policy(f'checkpoints/{env_name}.ckpt')
 
     # Render a test episode
-    test_loop(env_name, agent)
-
-    agent.save_policy(f'checkpoints/{env_name}.ckpt')
+    test_loop(env_name, agent)    
 
 if __name__ == '__main__':
-    main(T=500)
+    main()
--- utils/test.py
+++ utils/test.py
@@ -5,14 +5,22 @@ def test_loop(
     env_name: str,
     agent: BaseAgent,
     T: int = 1000,
+    S: int = 5,
 ):
-    render_env = gym.make(env_name, render_mode='human')
-    state, _ = render_env.reset(seed=42)
-    for _ in range(T):
-        render_env.render()
-        action = agent.policy.select_action(state)[0]
-        state, _, terminated, truncated, _ = render_env.step(action)
-        if terminated or truncated:
-            state, _ = render_env.reset()
-    render_env.close()
+    env = gym.make(env_name, render_mode='human')
+    state, _ = env.reset()
+    for _ in range(S):
+        for _ in range(T):
+            env.render()
+            if hasattr(agent, 'select_action'):
+                action = agent.select_action(state)
+            elif hasattr(agent, 'policy') and hasattr(agent.policy, 'select_action'):
+                action = agent.policy.select_action(state)[0]
+            else:
+                raise AttributeError('Agent does not have a valid `select_action` method.')
 
+            state, _, terminated, truncated, _ = env.step(action)
+            if terminated or truncated:
+                state, _ = env.reset()
+                break
+    env.close()
